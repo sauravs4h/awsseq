052d5f17124afae8a7123adf50314c93a993c86c
sauravs4h <sauravs4h@gmail.com>
2023-03-17 09:14:27 +0530

    sms

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const express = require("express")
-const { sequelize } = require("./config/db")
-const { userRouter } = require("./routes/user.routes")
+// const { sequelize } = require("./config/db")
+// const { userRouter } = require("./routes/user.routes")
 const app = express()
 
 app.use(express.json())
-app.use("/users",userRouter)
+// app.use("/users",userRouter)
 
 app.get("/",(req,res)=>{
     res.send("base api")
 })
 
-
-
 app.listen(8000,async()=>{
-    try {
-        await sequelize.authenticate()
-        await sequelize.sync()
-        console.log("connected")
-    } catch (error) {
-        console.log(error)
-    }
+    // try {
+    //     await sequelize.authenticate()
+    //     await sequelize.sync()
+    //     console.log("connected")
+    // } catch (error) {
+    //     console.log(error)
+    // }
     console.log("listening on port 8080")
 })
