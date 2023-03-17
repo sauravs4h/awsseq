c0329047ca097fe216b6a11664e8cf2cdb55b9e3
sauravs4h <sauravs4h@gmail.com>
2023-03-17 17:47:06 +0530

    sms

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const {Sequelize} = require("sequelize")
 
-const sequelize = new Sequelize("march15","root","Saurav@07",{
-    host:"localhost",
+const sequelize = new Sequelize("classroom","admin","123456789",{
+    host:"database-1.cdb4xuejm3sx.ap-south-1.rds.amazonaws.com",
+    //host:"localhost",
     dialect:"mysql"
 })
 
